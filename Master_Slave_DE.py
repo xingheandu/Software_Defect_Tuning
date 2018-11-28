eb881614a66d21fafe7e2b1025d3f1263e793147
Rui Shu <terry.shu@hotmail.com>
2018-11-28 15:42:12 -0500

    update master-slave

--- Master_Slave_DE.py
+++ Master_Slave_DE.py
@@ -1,21 +1,49 @@
 import multiprocessing as mp
+import pandas as pd
 import random, time
 from time import sleep
 import numpy as np
 import platform
 import logging, os
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, \
+    confusion_matrix
 
 # Define an output queue
 output = mp.Queue()
+DATASET_PATH = r"C:\Users\Terry\Documents\Software_Defect_Tuning\testDataset\ant-1.3.csv"
 
 global bounds, popsize, dimensions, pop, min_b, max_b, diff, pop_denorm, fitness, best, best_idx, mut, crossp, its
 
 
-def fobj(x):
-    value = 0
-    for i in range(len(x)):
-        value += x[i] ** 2
-    return value / len(x)
+def read_data(path):
+    data = pd.read_csv(path, skipinitialspace=True)
+    data = data.iloc[:, 3:]
+    return data
+
+
+def data_statistics(dataset):
+    # print the first 5 rows of data
+    print(dataset.head(5))
+    print('The shape of our dataset is:', dataset.shape)
+    # Descriptive statistics for each column
+    print(dataset.describe())
+
+
+def split_dataset(dataset, train_percentage):
+    X = dataset.iloc[:, :-1]
+    y = dataset.iloc[:, -1].map(lambda x: 'False' if x == 0 else 'True')
+    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=train_percentage, random_state=42)
+    return train_x, test_x, train_y, test_y
+
+
+# def fobj(x):
+#     value = 0
+#     for i in range(len(x)):
+#         value += x[i] ** 2
+#     return value / len(x)
 
 
 def de_initialization(fobj, bounds, popsize=60):
@@ -30,24 +58,36 @@ def de_initialization(fobj, bounds, popsize=60):
     return dimensions, pop, min_b, diff, best, best_idx, fitness
 
 
-def de_innerloop(output, its, popsize, pop, mut, dimensions, crossp, min_b, diff, lock, fitness, best_idx, best):
+def de_innerloop(output, its, popsize, pop, mut, dimensions, crossp, min_b, diff, lock, fitness, best_idx, best, train_x, test_x, train_y, test_y):
     for i in range(its // mp.cpu_count()):
         for j in range(popsize):
             idxs = [idx for idx in range(popsize) if idx != j]
             a, b, c = pop[np.random.choice(idxs, 3, replace=False)]
-            mutant = np.clip(a + mut * (b - c), 0, 1)
+            mutant = a + mut * (b - c)
+            for i, v in enumerate(mutant):
+                if 0 < v < 1: continue
+                if v < 0: mutant[i] = v + 1
+                if v > 1: mutant[i] = v - 1
+            # mutant = np.clip(res, 0, 1)
+
             cross_points = np.random.rand(dimensions) < crossp
             if not np.any(cross_points):
                 cross_points[np.random.randint(0, dimensions)] = True
+
             trial = np.where(cross_points, mutant, pop[j])
             trial_denorm = min_b + trial * diff
-            f = fobj(trial_denorm)
+            trail_denorm_convert = trial_denorm.tolist()
+            f = rf_tuning(np.int_(np.round_(trail_denorm_convert[0])), np.int_(np.round_(trail_denorm_convert[1])),
+                          np.int_(np.round_(trail_denorm_convert[2])),
+                          np.int_(np.round_(trail_denorm_convert[3])), float('%.2f' % trail_denorm_convert[4]),
+                          np.int_(np.round_(trail_denorm_convert[5])), train_x, test_x, train_y, test_y)
+
             lock.acquire()
-            f = fobj(trial_denorm)
-            if f < fitness[j]:
+            # f = fobj(trial_denorm)
+            if f > fitness[j]:
                 fitness[j] = f
                 pop[j] = trial
-                if f < fitness[best_idx]:
+                if f > fitness[best_idx]:
                     best_idx = j
                     best = trial_denorm
             lock.release()
@@ -85,6 +125,28 @@ def de_sequence(fobj, bounds, mut=0.8, crossp=0.9, popsize=60, its=3000):
         yield best, fitness[best_idx]
 
 
+def rf_tuning(n_estimators, min_samples_leaf, min_samples_split, max_leaf_nodes, max_features, max_depth, train_x, test_x, train_y, test_y):
+    """
+    Define the tuning target function, e.g., to achieve higher precision score in RandomForest
+    """
+    rf_tuning = RandomForestClassifier(n_estimators=n_estimators, min_samples_leaf=min_samples_leaf,
+                                       min_samples_split=min_samples_split, max_leaf_nodes=max_leaf_nodes,
+                                       max_features=max_features, max_depth=max_depth)
+    rf_tuning.fit(train_x, train_y)
+    predictions = rf_tuning.predict(test_x)
+    recall = recall_score(test_y, predictions, average="macro")
+    return recall
+
+
+def mlpn_tuning(alpha, learning_rate_init, power_t, max_iter, momentum, n_iter_no_change, train_x, test_x, train_y, test_y):
+    mlpn = MLPClassifier(alpha=alpha, learning_rate_init=learning_rate_init, power_t=power_t, max_iter=max_iter,
+                         momentum=momentum, n_iter_no_change=n_iter_no_change, solver="sgd")
+    mlpn.fit(train_x, train_y)
+    predictions = mlpn.predict(test_x)
+    recall = recall_score(test_y, predictions, average="macro")
+    return recall
+
+
 def main():
     # send it all to stderr
     mp.log_to_stderr()
@@ -92,36 +154,61 @@ def main():
     logger = mp.get_logger()
     logger.setLevel(logging.INFO)
 
-    print("--- Testing Sequence DE ---")
-    start_time_seq = time.time()
-    result_seq = list(de_sequence(fobj, bounds=[(-100, 100)] * 6))
-    print(result_seq[-1])
-    print("")
-    print("--- %s seconds ---" % (time.time() - start_time_seq))
+    dataset = read_data(DATASET_PATH)
+    # global train_x, test_x, train_y, test_y
 
-    sleep(5)
+    train_x, test_x, train_y, test_y = split_dataset(dataset, 0.25)
 
+    # print("--- Testing Sequence DE ---")
+    # start_time_seq = time.time()
+    # result_seq = list(de_sequence(fobj, bounds=[(-100, 100)] * 6))
+    # print(result_seq[-1])
+    # print("")
+    # print("--- %s seconds ---" % (time.time() - start_time_seq))
+    #
+    # sleep(5)
+
+    print("--- Tuning Random Forest with Parallel DE ---")
     start_time_para = time.time()
-    print("--- Testing Parallel DE ---")
 
     # result_para = list(de_parallel(fobj, bounds=[(-100, 100)] * 6))
     # print(result_para[-1])
 
     # initialization
-    bounds = [(-100, 100)] * 6
-    popsize = 200
+    bounds = [(10, 150), (1, 20), (2, 20), (2, 50), (0.01, 1), (1, 10)]
     mut = 0.8
-    crossp = 0.9
+    crossp = 0.7
     popsize = 60
-    its = 3000
+    its = 100
 
     dimensions = len(bounds)
     pop = np.random.rand(popsize, dimensions)
+
+    # pdb.set_trace()
     min_b, max_b = np.asarray(bounds).T
     diff = np.fabs(min_b - max_b)
     pop_denorm = min_b + pop * diff
-    fitness = np.asarray([fobj(ind) for ind in pop_denorm])
-    best_idx = np.argmin(fitness)
+
+    # convert from float to integer
+    pop_denorm_convert = pop_denorm.tolist()
+
+    result_list = []
+    temp_list = []
+
+    for index in pop_denorm_convert:
+        temp_list.append(np.int_(np.round_(index[0])))
+        temp_list.append(np.int_(np.round_(index[1])))
+        temp_list.append(np.int_(np.round_(index[2])))
+        temp_list.append(np.int_(np.round_(index[3])))
+        temp_list.append(float('%.2f' % index[4]))
+        temp_list.append(np.int(np.round_(index[5])))
+        result_list.append(temp_list)
+        temp_list = []
+
+    fitness = np.asarray([rf_tuning(index[0], index[1], index[2], index[3], index[4], index[5], train_x, test_x, train_y, test_y)
+                          for index in result_list])
+
+    best_idx = np.argmax(fitness)
     best = pop_denorm[best_idx]
 
     print("Dimension:", dimensions)
@@ -139,7 +226,7 @@ def main():
     processes = []
     for x in range(mp.cpu_count()):
         processes.append(mp.Process(target=de_innerloop, args=(
-            output, its, popsize, pop, mut, dimensions, crossp, min_b, diff, lock, fitness, best_idx, best)))
+            output, its, popsize, pop, mut, dimensions, crossp, min_b, diff, lock, fitness, best_idx, best, train_x, test_x, train_y, test_y)))
 
     # Run processes
     for p in processes:
@@ -156,6 +243,8 @@ def main():
 
     print("")
     print("--- %s seconds ---" % (time.time() - start_time_para))
+
+    print("--- Tuning Multilayer Perceptron with Parallel DE ---")
 
 
 if __name__ == "__main__":
